0617d200a92689f7ad5b705654d03cf0f3ca2886
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-10-27 01:25:19 +0300

    Task 1 ready

---
+++ main.py
@@ -0,0 +1,17 @@
+#task 1
+# import random
+#
+# def create_random_list(length, min_num, max_num):
+#     return [random.randint(min_num, max_num) for _ in range(length)]
+#
+# def multiply_list_nums(input_list):
+#     result = 1
+#     for num in input_list:
+#         result *= num
+#     return result
+#
+# random_list = create_random_list(7, 1, 10)
+# product = multiply_list_nums(random_list)
+#
+# print(f"Our list: {random_list}")
+# print(f"Product of numbers in the list: {product}")
