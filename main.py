605f4596306d4452d3dcf65cf9f532648a4896f9
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-10-27 01:54:34 +0300

    Task 2 ready

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#task 1
+# task 1: Напишіть функцію, яка обчислює добуток елементів списку цілих.
+# Список передається як параметр. Отриманий результат повертається із функції.
+
 # import random
 #
 # def create_random_list(length, min_num, max_num):
@@ -15,3 +17,20 @@
 #
 # print(f"Our list: {random_list}")
 # print(f"Product of numbers in the list: {product}")
+
+# task 2 Напишіть функцію для знаходження мінімуму у списку цілих.
+# Список передається як параметр. Отриманий результат повертається із функції.
+# import random
+#
+# def create_random_list(length, min_num, max_num):
+#     return [random.randint(min_num, max_num) for _ in range(length)]
+#
+# def find_min_num(input_list):
+#     min_num = min(input_list)
+#     return(min_num)
+#
+# random_list = create_random_list(7, 1, 10)
+# min_num = find_min_num(random_list)
+#
+# print(f"Our list: {random_list}")
+# print(f"Minimal number: {min_num}")
